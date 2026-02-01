436049bb2a16137c62e02ccf281fd7308053bf84
andrey <andrey.mmendonca@gmail.com>
2026-02-01 16:26:41 -0300

    BackEnd projeto inicial

---
+++ backend/src/main/java/com/senai/backend/CadastroDePatrimonioApplication.java
@@ -0,0 +1,13 @@
+package com.senai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDePatrimonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDePatrimonioApplication.class, args);
+	}
+
+}

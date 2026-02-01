7ee31eca4bd1e14d54e2d5445aa90d65dc6c7d33
andrey <andrey.mmendonca@gmail.com>
2026-02-01 19:25:53 -0300

    feat: entidade patrimonio

---
+++ backend/src/main/java/com/senai/backend/entities/PatrimonioEntity.java
@@ -0,0 +1,56 @@
+package com.senai.backend.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_patrimonio")
+public class PatrimonioEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "patrimonio_id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "numero_patrimonio")
+    private String numeroPatrimonio;
+
+    @ManyToOne
+    @JoinColumn(name = "setor_id")
+    private SetorEntity setor;
+
+    public PatrimonioEntity(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumeroPatrimonio() {
+        return numeroPatrimonio;
+    }
+
+    public void setNumeroPatrimonio(String numeroPatrimonio) {
+        this.numeroPatrimonio = numeroPatrimonio;
+    }
+
+    public SetorEntity getSetor() {
+        return setor;
+    }
+
+    public void setSetor(SetorEntity setor) {
+        this.setor = setor;
+    }
+}

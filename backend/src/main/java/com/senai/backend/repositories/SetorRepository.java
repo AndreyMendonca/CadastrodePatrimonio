ee7e96b05cec6cb6348507da6a9c04cc0e7ca385
andrey <andrey.mmendonca@gmail.com>
2026-02-01 16:43:18 -0300

    feat: repository setor

---
+++ backend/src/main/java/com/senai/backend/repositories/SetorRepository.java
@@ -0,0 +1,7 @@
+package com.senai.backend.repositories;
+
+import com.senai.backend.entities.SetorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SetorRepository extends JpaRepository<SetorEntity, Long> {
+}
